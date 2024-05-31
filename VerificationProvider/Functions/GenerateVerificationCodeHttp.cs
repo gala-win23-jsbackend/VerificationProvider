e95e815f49f0df92d86081b9fa756d6c13f0d549
Gala <70331948+galinamiteva@users.noreply.github.com>
2024-05-31 22:15:57 +0200

    add new func Generate Code HTTP

---
+++ VerificationProvider/Functions/GenerateVerificationCodeHttp.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using VerificationProvider.Models;
+using VerificationProvider.Services;
+
+namespace VerificationProvider.Functions;
+
+public class GenerateVerificationCodeHttp
+{
+    private readonly ILogger<GenerateVerificationCodeHttp> _logger;
+    private readonly IVerificationService _verificationService;
+
+    public GenerateVerificationCodeHttp(ILogger<GenerateVerificationCodeHttp> logger, IVerificationService verificationService)
+    {
+        _logger = logger;
+        _verificationService = verificationService;
+    }
+
+    [Function("GenerateVerificationCodeHttp")]
+    public async Task<IActionResult> RunAsync([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        try
+        {
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var verificationRequest = JsonConvert.DeserializeObject<VerificationRequest>(requestBody);
+
+            if (verificationRequest != null)
+            {
+                var code = _verificationService.GenerateCode();
+                if (!string.IsNullOrEmpty(code))
+                {
+                    var result = await _verificationService.SaveVerificationRequest(verificationRequest, code);
+                    if (result)
+                    {
+                        var emailRequest = _verificationService.GenerateEmailRequest(verificationRequest, code);
+                        if (emailRequest != null)
+                        {
+                            string payload = _verificationService.GenerateServiceBusEmailRequest(emailRequest);
+                            if (!string.IsNullOrEmpty(payload))
+                            {
+                                return new OkObjectResult(payload);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError($"ERROR : GenerateVerificationCodeHttp.Run :: {ex.Message}");
+            return new StatusCodeResult(StatusCodes.Status500InternalServerError);
+        }
+
+        return new BadRequestResult();
+    }
+}
